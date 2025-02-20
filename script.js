6a070f858e00d48cb72b60d51021c486e9bcf3d6
agaroma <panterande@gmail.com>
2025-02-20 06:58:57 +0200

    section__services delete

--- script.js
+++ script.js
@@ -23,14 +23,3 @@ let qa__wrapper = document.querySelectorAll('.qa__wrapper')
             });
 
         });
-
-        document.addEventListener("DOMContentLoaded", function() {
-          let counter = 1;
-          setInterval(function() {
-            document.getElementById('radio' + counter).checked = true;
-            counter++;
-            if (counter > 4) {
-              counter = 1;
-            }
-          }, 5000);
-        });
